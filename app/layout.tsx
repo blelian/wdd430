43cddf98876e1f281f2cf36707a652993ff6a79b
blelian <bullsurus@gmail.com>
2025-09-15 10:41:13 +0200

    coorections to try and fix vercel deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/ui/global.css';
 import { Inter } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export default function RootLayout({
   children,
