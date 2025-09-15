656fa264b3f050d0eb45344492cebe75745a462b
blelian <bullsurus@gmail.com>
2025-09-15 10:34:08 +0200

    Small changes just so vercel sees latest changes i made

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
- 
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
+      <body className="antialiased" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
